59dd23489fc76133c2065d78f74747186f2f3fd0
Gabedini <107578050+Gabedini@users.noreply.github.com>
2023-03-15 10:51:54 -0500

    Two reference files to get us started

---
+++ Classic API Basic Commands Reference.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import requests
+import json
+
+username = ''
+password = ''
+url = 'https://INSTANCE_NAME_HERE.jamfcloud.com/'
+
+
+"""———————————————————————————————————————"""
+"""About as basic as it gets. Note: Classic API Defualts XML return."""
+"""———————————————————————————————————————"""
+response = requests.get(url + "JSSResource/computers", auth = (username, password))
+content = response.text
+print(content)
+
+
+"""———————————————————————————————————————"""
+"""About as basic as it gets, except we ask for json formatting"""
+"""———————————————————————————————————————"""
+response = requests.get(url + "JSSResource/mobiledevices", auth = (username, password), headers = {"Accept": "application/json"})
+jsoncontent = response.json()
+print(jsoncontent)
+
+
+"""———————————————————————————————————————"""
+"""POSTing an item and supplying the XML for it"""
+"""———————————————————————————————————————"""
+xmlData = "<category><name>Getting Started with Python</name><priority>1</priority></category>"
+response = requests.post(url + "JSSResource/categories", auth = (username, password), data = xmlData )
+content = response.text
+print(content)
+
+
+"""———————————————————————————————————————"""
+"""PUTing computer of ID 8 into a different site using json
+Just kidding, you can't do that with the classic API, only XML"""
+"""———————————————————————————————————————"""
+modidfyComputerSite = "<computer><general><site><name>Eau Claire</name></site></general></computer>"
+response = requests.put(url + "JSSResource/computers/id/8", auth = (username, password), headers = {"Accept": "application/json"}, data = modidfyComputerSite )
+content = response.text
+print(content)
+
+
+"""———————————————————————————————————————"""
+"""I intentionally did not include a DELETE,
+my reasoning is that you should be able to figure that out based on the other commands
+and _only_ should be running it if you are comfortable enough to produce your own solution
+Never test in prod, especially if you have a loop of some sort"""
+"""———————————————————————————————————————"""
+
+
+"""———————————————————————————————————————"""
+"""A requests session should help speed things up when doing multiple calls.
+However, it may also present other considerations when doing so, as such, it may not be worth the performance gain in *all* scenarios
+(This would be accompanied by a session.get instead of requests.get)"""
+"""———————————————————————————————————————"""
+session = requests.Session()
+
+
+"""———————————————————————————————————————"""
+"""Getting a bearer token and proving it works with a simple GET"""
+"""———————————————————————————————————————"""
+response = requests.post(url + "api/v1/auth/token", auth = (username, password))
+response_data = response.json()
+"""We can call the specific json value 'token' with this formatting, run a print on the token and response.json values if you want to see it in action"""
+token = response_data["token"]
+"""note the f string to include Bearer, that is needed, example https://docs.jamf.com/education-services/resources/20230120/Resources_300_Lesson_10.html"""
+head = {'Authorization': f'Bearer {token}' }
+response = requests.get(url + "JSSResource/networksegments", headers=head )
+content = response.text
+print(content)
+
+
+
+
+
+
+
---
+++ Jamf Pro API Basic Commands Reference.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import requests
+
+
+username = ''
+password = ''
+url = 'https://NSTANCE_NAME_HERE.jamfcloud.com/api/v1/'
+
+session = requests.Session()
+
+"""———————————————————————————————————————"""
+"""We will use token auth like last time. Doing a simple GET here"""
+"""———————————————————————————————————————"""
+response = session.post(url + "auth/token", auth = (username, password))
+response_data = response.json()
+token = response_data["token"]
+head = {'Authorization': f'Bearer {token}' }
+"""Building on the Classic API examples, we're going to make some more 're-usable' formatting here with creating functions
+The below is a simple GET
+Realistically, this isn't the most useful task to use in this way, but we're still going to practice it here
+Notice that the Jamf Pro API uses json formatting"""
+def getAnItem(urlForCall, dataForHeader):
+	response = session.get(urlForCall + "advanced-mobile-device-searches", headers=dataForHeader )
+	content = response.text
+	print(content)
+getAnItem( url, head)
+
+
+"""———————————————————————————————————————"""
+"""Time to POST an item"""
+"""———————————————————————————————————————"""
+jsonData = {"name":"Made by API2","priority":"9"}
+def postAnItem(urlForCall, dataForHeader, dataForNewItem):
+	"""Thing to notice in this one is the use of the json parameter to encode it instead of data"""
+	response = session.post(urlForCall + "categories", headers=dataForHeader, json = dataForNewItem )
+	content = response.text
+	print(content)
+postAnItem(url, head, jsonData)
+
+
+"""———————————————————————————————————————"""
+"""Time to PUT an item. We are simply updating a few fields for script 16."""
+"""———————————————————————————————————————"""
+jsonData = {"name": "Updated Name via API", "notes": "using the API to PUT this here"}
+def putAnItem(urlForCall, dataForHeader, dataForNewItem):
+	response = session.put(urlForCall + "scripts/16", headers=dataForHeader, json = dataForNewItem )
+	content = response.text
+	print(content)
+putAnItem(url, head, jsonData)
+
+
+"""———————————————————————————————————————"""
+"""I intentionally did not include a DELETE,
+my reasoning is that you should be able to figure that out based on the other commands
+and _only_ should be running it if you are comfortable enough to produce your own solution
+Never test in prod, especially if you have a loop of some sort"""
+"""———————————————————————————————————————"""
+
+
+
+
+
