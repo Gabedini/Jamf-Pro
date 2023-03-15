b97c73567c6f8641ef0987ed0bd82daa631070d6
Gabedini <agabedini@gmail.com>
2023-03-15 11:12:54 -0500

    just making sure that a push works

---
+++ fun project.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import requests
+
+username = ''
+password = ''
+url = 'https://INSTANCE_NAME_HERE.jamfcloud.com/'
+"""This is a placeholder file for a fun project being worked on"""
