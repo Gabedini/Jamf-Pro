0f5893c7c7e9781c704b94bd6183bc76194a5e94
Gabedini <agabedini@gmail.com>
2023-03-29 11:50:09 -0500

    we've got logging, 1 error handled, updated logic

--- fun project.py
+++ fun project.py
@@ -9,26 +9,42 @@ password = ''
 """This is a placeholder file for a fun project being worked on"""
 session = requests.Session()
 logs = open("/tmp/LAPSTool.log", "a")
+global clientManagementId
+clientManagementId = ""
 
 """This method gets us a bearer token from Jamf Pro."""
 def getToken(url, jpUser, jpPass):
-	response = session.post(url + "auth/token", auth = (jpUser, jpPass))
-	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {url}auth/token")
-	logs.write(response.text)
-	responseData = response.json()
-	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {str(responseData)}")
-	token = responseData["token"]
-	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} This is the token: {token}")
-	logs.write("")
-	return token
+	try:
+		response = session.post(url + "auth/token", auth = (jpUser, jpPass))
+		logs.write(f"{datetime.now().strftime(' %Y-%m-%d %H:%M:%S')} Getting token from: {url}auth/token")
+		logs.write(response.text)
+		responseData = response.json()
+		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Reply: {str(responseData)}")
+		token = responseData["token"]
+		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} This is the token: {token}")
+		return token
+	except requests.exceptions.MissingSchema as error:
+		errorMsg = str(error)
+		print(f"testing print of error {errorMsg}")
+		return errorMsg
 
 """Grabs the current settings in Jamf Pro"""
 def getCurrentSettings(url, dataForHeader):
 	response = session.get(url + "local-admin-password/settings", headers=dataForHeader )
-	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {response.text}")
+	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Collecting Current settings: {response.text}")
 	currentSettings = response.json()
 	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} returning current settings {currentSettings}")
 	return currentSettings
+
+def getManagmentID(url, dataForHeader, computerID):
+	global clientManagementId
+	response = session.get(url + f"computers-inventory-detail/{computerID}", headers=dataForHeader )
+	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Collecting Managment ID for: {url}computers-inventory-detail/{computerID}")
+	content = response.json()
+	clientManagementId = content["general"]["managementId"]
+	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Collected managemend ID: {clientManagementId}")
+	return clientManagementId
+
 
 """Enables LAPS if disabled"""
 def enableIfDisabled(url, dataForHeader):
@@ -53,38 +69,49 @@ I'll probably just make this a button and then mention that in the GUI somewhere
 
 
 """Get LAPS password viewed history. (returns the whole json for formatting later)"""
-def getViewedHistory(url, dataForHeader, clientManagementId, username):
-	if clientManagementId == "" or username == "":
-		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Client Management ID or Username")
-		return "Missing Client Management ID or Username"
+def getViewedHistory(url, dataForHeader, computerID, username):
+	if computerID == "" or username == "":
+		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Computer ID or Username")
+		return "Missing Computer ID or Username"
 	else:
-		response = session.get(url + f"local-admin-password/{clientManagementId}/account/{username}/audit", headers=dataForHeader )
+		if clientManagementId == "":
+			logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Client Management ID, collecting...")
+			clientManagementID = getManagmentID(jpURL, head, computerID)
+		response = session.get(url + f"local-admin-password/{computerID}/account/{username}/audit", headers=dataForHeader )
 		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} History collection response: {response}")
 		history = response.json()
 		print(f"is it running the history function? {history}")
 		return history
 
 """Get current LAPS password for specified username on a client. (returns just the password)"""
-def getLAPSPassword(url, dataForHeader, clientManagementId, username):
-	if clientManagementId == "" or username == "":
-		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Client Management ID or Username")
-		return "Missing Client Management ID or Username"
+def getLAPSPassword(url, dataForHeader, computerID, username):
+	if computerID == "" or username == "":
+		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Computer ID or Username")
+		return "Missing Computer ID or Username"
 	else:
-		response = session.get(url + f"local-admin-password/{clientManagementId}/account/{username}/password", headers=dataForHeader )
+		if clientManagementId == "":
+			logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Client Management ID, collecting...")
+			clientManagementID = getManagmentID(jpURL, head, computerID)
+		response = session.get(url + f"local-admin-password/{computerID}/account/{username}/password", headers=dataForHeader )
 		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {response.text}")
 		content = response.json()
-		print(f"{url}local-admin-password/{clientManagementId}/account/{username}/password")
+		print(f"{url}local-admin-password/{computerID}/account/{username}/password")
 		print(content)
 		lapsPass = content["password"]
 		return lapsPass
 
 """Get the LAPS capable admin accounts for a device. (returns just the account name)"""
-def getLAPSAccount(url, dataForHeader, clientManagementId):
-	if clientManagementId == "":
-		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Client Management ID")
-		return "Missing Client Management ID"
+def getLAPSAccount(url, dataForHeader, computerID):
+	logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Getting LAPS Enabled Account for computer ID:  {computerID}")
+	if computerID == "":
+		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Computer ID")
+		return "Missing Computer ID"
 	else:
+		if clientManagementId == "":
+			logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Missing Client Management ID, collecting...")
+			clientManagementID = getManagmentID(jpURL, head, computerID)
 		response = session.get(url + f"local-admin-password/{clientManagementId}/accounts", headers=dataForHeader )
+		print(f"{url}local-admin-password/{clientManagementId}/accounts")
 		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Password collection response: {response}")
 		content = response.json()
 		logs.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {content}")
@@ -128,26 +155,26 @@ class App(customtkinter.CTk):
 		print("you clicked the enable laps button")
 
 	def lapsPass(self):
-		output = getLAPSPassword(jpURL, head, self.inputClientManagementId.get(), self.inputComputerUser.get())
+		output = getLAPSPassword(jpURL, head, self.inputComputerID.get(), self.inputComputerUser.get())
 		print(f"Printing the return from the collecting password button{output}")
 		self.outputBox.insert("insert", f"{output}" + "\n")
 		print("you clicked the collecting password button")
 
 	def gettingHistory(self):
-		output = getViewedHistory(jpURL, head, self.inputClientManagementId.get(), self.inputComputerUser.get())
+		output = getViewedHistory(jpURL, head, self.inputComputerID.get(), self.inputComputerUser.get())
 		print(f"Printing the return from the getting laps history button{output}")
 		self.outputBox.insert("insert", f"{output}" + "\n")
 		print("you clicked the getting laps history button")
 
 	def lapsAccount(self):
-		output = getLAPSAccount(jpURL, head, self.inputClientManagementId.get())
+		output = getLAPSAccount(jpURL, head, self.inputComputerID.get())
 		print(f"Printing the return from the getting account button{output}")
 		self.outputBox.insert("insert", f"{output}" + "\n")
 		print("you clicked the getting account button")
 
 	def optionPage(self):
-		self.inputClientManagementId = customtkinter.CTkEntry(master=self, placeholder_text="Client Management ID")
-		self.inputClientManagementId.grid(row=0, column=0, pady=12, padx=10)
+		self.inputComputerID = customtkinter.CTkEntry(master=self, placeholder_text="Computer ID")
+		self.inputComputerID.grid(row=0, column=0, pady=12, padx=10)
 
 		self.inputComputerUser = customtkinter.CTkEntry(master=self, placeholder_text="Local Admin Acount")
 		self.inputComputerUser.grid(row=0, column=1, pady=12, padx=10)
@@ -166,7 +193,6 @@ class App(customtkinter.CTk):
 
 		self.outputBox = customtkinter.CTkTextbox(master=self)
 		self.outputBox.grid(row=3, column=0, columnspan=2, padx=20, pady=20)
-
 
 
 	def userLogin(self):
@@ -191,6 +217,10 @@ class App(customtkinter.CTk):
 		token = getToken(jpURL, username, password)
 		print(token)
 		"""puts token in header for us to pass later"""
+		if token.startswith("Invalid URL") == True:
+			self.outputBox = customtkinter.CTkTextbox(master=self)
+			self.outputBox.pack(padx=20, pady=20)
+			self.outputBox.insert("insert", f"{token}")
 		head = {'Authorization': f'Bearer {token}' }
 
 		"""gets and returns whatever the current settings are"""
@@ -213,6 +243,7 @@ class App(customtkinter.CTk):
 		self.inputUsernm.pack_forget()
 		self.inputPasswd.pack_forget()
 		self.inputURL.pack_forget()
+		self.outputBox.pack_forget()
 		self.optionPage()
 
 
